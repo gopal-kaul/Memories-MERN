11d40180b3791431b3396999b0e8205d4b0a69cf
Gopal Kaul <gopalkaul5@gmail.com>
2021-02-08 09:34:22 +0530

    Some more work on Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
-
-function App() {
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import Posts from './Components/Posts/Posts.js'
+import Form from './Components/Forms/Forms'
+const App = () => {
   return (
-    <div className="App">
-    </div>
-  );
+    <Container maxwidth='lg'>
+      <AppBar position='static' color='inherit'>
+        <Typography variant='h2' align='center'>
+          Memories
+        </Typography>
+        <Grow in>
+          <Container>
+            <Grid container justify='space-between' alignItems='stretch' spacing={4}>
+              <Grid item xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} sm={7}>
+                <Form />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </AppBar>
+    </Container>
+  )
 }
 
 export default App;
